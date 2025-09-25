6503d92fce07361e59971a66d29761e1bfcbdabf
kusSsa <kuskovaevgenia3083@gmail.com>
2025-09-25 12:09:12 +0500

    Добавила компоненты страниц

---
+++ src/components/app.tsx
@@ -0,0 +1,14 @@
+﻿import { JSX } from 'react';
+import MainScreen from './main-screen.tsx';
+
+type AppScreenProps = {
+  cardCount: number;
+}
+
+function App({cardCount}: AppScreenProps) : JSX.Element {
+  return (
+    <MainScreen cardCount={cardCount}/>
+  );
+}
+
+export default App;
