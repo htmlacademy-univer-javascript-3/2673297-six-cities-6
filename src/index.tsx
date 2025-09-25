bfe9b5aaa9db1e977263dfe11c4f4b2e5679c07f
kusSsa <kuskovaevgenia3083@gmail.com>
2025-09-25 18:56:40 +0500

    try again

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App/App.tsx';
+import App from './reactComponents/App/App.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
